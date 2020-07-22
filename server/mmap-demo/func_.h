2f53703774332cb3ef8a76a3a85399f84f5bd250
joshuasun <sjs>
2020-07-22 23:23:27 +0800

    demo mmap ???

---
+++ server/mmap-demo/func_.h
@@ -0,0 +1,24 @@
+#ifndef __FUNC__
+#define __FUNC__
+#include <unistd.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <thread>
+
+
+
+void mmapcopy(int src_fd, size_t src_len, int dst_fd, off_t offset = 0);
+
+void mergeFile(int src_fd_1, int src_fd_2, size_t src_len_sum,const char* output);
+
+
+#endif // !__MMAP_TEST__
