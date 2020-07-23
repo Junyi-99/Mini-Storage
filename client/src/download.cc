3867fbe8d58fc143c3fffa8afb688c4cdd3004a6
fufuzhao <fufuzhao@tencent.com>
2020-07-23 17:33:44 +0800

    feat: download request file_size then do...

--- client/src/download.cc
+++ client/src/download.cc
@@ -1,16 +1,27 @@
+#include "../../utils/tcp_socket.hpp"
 #include "../etc/config.h"
 #include "../include/big_file.h"
 #include "../include/small_file.h"
-#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // TODO: 向server请求文件大小
-  if (file_size > SMALL_FILE_SIZE_MAX) {
-    do_big_file_download(argv[1], file_size);
-  } else {
-    do_small_file_download(argv[1], file_size);
+  if (argc == 0) {
+    std::cout << "error   =>>   文件名??" << std::endl;
+    return 0;
   }
-  // printf("%s   %d \n",argv[1], file_size);
-  close(fd);
-  exit(0);
+  // request file_size(uint64_t)
+  char *file_name = argv[1];
+  TcpSocket socket_fd = TcpSocket();
+  socket_fd.Socket();
+  socket_fd.Connect(SERVER_IP_ADDR_1, SERVER_PORT);
+  socket_fd.Send((void *)file_name, strlen(file_name));
+  uint64_t file_size;
+  socket_fd.Recv((void *)&file_size, sizeof(file_size));
+
+  if (file_size > SMALL_FILE_SIZE_MAX) {
+    do_big_file_download(file_name, file_size);
+  } else {
+    // do_small_file_download(argv[1], file_size);
+  }
+  socket_fd.Close();
+  return 0;
 }
