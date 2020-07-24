1cace9cfdd76d8909ff50202130bea6a401d9281
joshuasun <sjs>
2020-07-24 22:56:32 +0800

    io operation function

--- server/include/file.h
+++ server/include/file.h
@@ -1,27 +1,63 @@
-//
-// Created by Junyi Hou on 7/21/20.
-//
+#ifndef __FILE__SYSTEM__
+#define __FILE__SYSTEM__
+#include<iostream>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <errno.h>
+//#include <sys/types.h>
+//#include <assert.h>
 
-#ifndef FUCK_STORAGE_SERVER_FILE_H
-#define FUCK_STORAGE_SERVER_FILE_H
 
-#include <cstdint>
+#define ROOT_PATH "/raid/"
 
-// 获取文件大小
-// 失败返回 -1
-uint32_t file_size(const char *filename);
 
-// 获取文件【读】指针
-// 成功返回 mmap映射的地址，失败返回 nullptr
-void *file_get(int disk_id, const char *filename);
+//totalFile的文件读写提供了两个单独的函数：
+//  储存totalFile大小： file_set(filename, filesize); 
+//  获取totalFile大小： getTotalSize(filename);
 
-// 设置文件基本信息
-void file_set(const char *filename, uint32_t filesize);
 
-// 磁盘编号 [0,12), 文件名，要写的数据缓冲区，要写多少字节
-int file_write(int disk_id, const char *filename, void *buffer, uint32_t length);
 
-// 挂载磁盘（防止服务器磁盘没有挂载）
-int disk_mount(int disk_id);
+//用于读写文件的封装类：
+/*
+声明格式：
+    1. 读取模式  File_Opt read(filename)
+        1.1 读取全部内容:  read.read_all()
+        1.2 按偏移量和字符数读取内容: read.read_part(off_set, NumOfChar) 
+        1.3 提供读取该分块对应totalFile的文件大小: read.get_total_file_size();
+            (也可以使用 getTotalSize(filename) 函数来读取)
+    2. 写入模式  File_Opt write(filename, filesize)
+        只提供 按照内容 和 偏移量 写入的接口： write.write_by_offset(content, off_set);
+*/
+class File_Opt {
+public:
+    File_Opt(const char* filename, off_t filesize); //写文件时，需要提供：文件名 及 文件大小
+    File_Opt(const char* filename); //读文件时，只需传入：文件名
+    ~File_Opt();
+    void write_by_offset(const char* content, off_t off_set);   //写入文件，需要提供：内容 及 偏移量
+    char* read_all();   //读取全部内容
+    char* read_part(off_t off_set, off_t NumOfChar);    //读取部分内容，需要提供：偏移量 及 字符个数
+    int get_total_file_size();
+private:
+    int fd;
+    void* mmap_addr;
+    char* operation_postion;
+    const char* file_name;
+    const char* file_path;
+    off_t file_size;
+    off_t total_file_size;
+    char* mmap_write_byte_part(char* addr, const char* content);
+};
 
-#endif //FUCK_STORAGE_SERVER_FILE_H
+//记录total文件大小
+int file_set(const char* filename, uint32_t filesize);
+
+//获取total文件大小
+off_t getTotalSize(const char* filename);
+
+#endif // !__FILE__SYSTEM__
--- server/src/file.cpp
+++ server/src/file.cpp
@@ -1,67 +1,191 @@
-//
-// Created by Junyi Hou on 7/21/20.
-//
-
 #include "../include/file.h"
 
-// 获取文件基本信息（文件名、大小、多少个块等等）
-// 失败返回 -1
-uint32_t file_size(const char *filename) {
+//写入文件模式时，创建对应文件，设置文件大小，建立映射
+File_Opt::File_Opt(const char* filename, off_t filesize) {
+    this->file_name = filename;
+    this->file_size = filesize;
+    this->total_file_size = -1;
+    std::string fatherPath = ROOT_PATH ;
+    std::string filePath = fatherPath + filename;// "/raid/${file_name}"
+    this->file_path = filePath.c_str();
+    if ((this->fd = open(file_path, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU)) < 0) {    //创建文件
+        printf("%s_info create failed! \n",filename);
+        //perror("creat file failed");
+    }
+    truncate(file_path, file_size);
+    this->mmap_addr = mmap(NULL, file_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0); 
+    if (mmap_addr == MAP_FAILED) {
+        printf("mmap error: %s\n", strerror(errno));
+        //perror("mmap create failed");
+    }
+};
+
+//读取模式时，打开对应文件，建立映射
+File_Opt::File_Opt(const char* filename) {
+    this->file_name = filename;
+    this->total_file_size = -1;
+    std::string fatherPath = ROOT_PATH ;
+    std::string filePath = fatherPath + filename;// "/raid/${file_name}"
+    this->file_path = filePath.c_str(); 
+    if ((this->fd = open(file_path, O_RDONLY)) < 0) {    //打开文件
+        printf("%s open failed! \n",filename);
+        //perror("creat file failed");
+    }
+    struct stat stat;
+    fstat(fd, &stat);               // 获取文件信息
+    file_size = stat.st_size;
+
+    this->mmap_addr = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0); 
+    if (mmap_addr == MAP_FAILED) {
+        printf("mmap error: %s\n", strerror(errno));
+        //perror("mmap create failed");
+    } 
+}
+
+//析构时，关闭文件，解除映射，落盘
+File_Opt::~File_Opt(){
+    if (close(fd) < 0) {
+        printf("close %s failed!\n",file_name);
+    }
+    if (munmap(mmap_addr,file_size) < 0) {
+        printf("close map of %s failed! \n",file_name);
+    }
+    if (msync(mmap_addr,file_size,MS_SYNC) < 0) {
+        //printf("%s write to disk failed! \n",file_name);
+    }
+}
+
+//文件系统自用工具函数
+/*
+mmap映射时，给出首地址和待储存内容，会自动储存到映射区域，并返回下一个可储存的位置
+*/
+char* File_Opt::mmap_write_byte_part(char* addr, const char* content){
+    int len = strlen(content);
+    char* opt_addr_ptr = addr;
+    memcpy(opt_addr_ptr,content,len);
+    opt_addr_ptr[len] = '\n';
+    opt_addr_ptr = opt_addr_ptr + len + 1;
+    return opt_addr_ptr;
+}
+
+//给出要储存的内容 和 该部分内容的偏移量即可
+void File_Opt::write_by_offset(const char* content, off_t off_set){
+    operation_postion = (char* )mmap_addr + off_set;
+    operation_postion = mmap_write_byte_part(operation_postion,content);
+}
+
+
+//读取全部文件内容
+char* File_Opt::read_all() {
+    char* res = new char[file_size];
+    memcpy(res,(char* )mmap_addr,file_size);
+    return res;
+}
+
+//读取部分文件内容，给出偏移量 和 长度
+char* File_Opt::read_part(off_t off_set, off_t NumOfChar) {
+    char* res = new char[NumOfChar];
+    memcpy(res,(char* )mmap_addr + off_set,NumOfChar);
+    return res;
+}
+
+//从小文件里面读取total文件信息
+int File_Opt::get_total_file_size() {
+    if (this->total_file_size < 0){
+        this->total_file_size = getTotalSize(this->file_name);
+    }
+    return this->total_file_size;
+}
+
+//读取total文件大小
+/*
+return error:
+    -1: 文件打开失败
+    -2：建立映射失败
+    -3：解除映射失败
+    -4：文件关闭失败
+    >0: 成功
+*/
+off_t getTotalSize(const char* filename) {
+    std::string fatherPath = ROOT_PATH ;
+    std::string fileinfoPath = fatherPath + filename + "_info"; // "/raid/${file_name}_info"
+    int info_fd;
+    if ((info_fd = open(fileinfoPath.c_str(), O_RDONLY)) < 0) {    //创建信息文件
+        printf("%s_info open failed! \n",filename);
+        return -1;
+    }
+    struct stat stat;
+    fstat(info_fd, &stat);               // 获取文件信息
+    off_t file_size = stat.st_size;
+    void* info_ptr = mmap(NULL, file_size, PROT_READ, MAP_SHARED, info_fd, 0); 
+    if (info_ptr == MAP_FAILED) {
+        printf("mmap error: %s\n", strerror(errno));
+        return -2;
+    }
+    char* res = new char[strlen((char*)info_ptr) + 1];
+    memcpy(res,(char*)info_ptr,strlen((char*)info_ptr));
+    off_t total_size = std::atoll(res);
+    delete[] res;
+    if (munmap(info_ptr,file_size) < 0) {
+        printf("unmap error: %s \n", filename);
+        return -3;
+    }
+    if (close(info_fd) < 0) {
+        printf("close %s failed \n",filename);
+        return -4;
+    }
+    return total_size;
+}
+
+
+
+//储存文件信息(是切块之前的文件的信息，即记录该文件一共有多大，用于client判断是大文件还是小文件)
+/*
+return error:
+    -1: 文件创建失败
+    -2：建立映射失败
+    -3：解除映射失败
+    -4：文件关闭失败
+    1: 成功
+*/
+int file_set(const char* filename,  uint32_t filesize) {
+    //不单独为每个文件创建文件夹，统一放在/raid下面
+    //总文件的信息记录在/raid/filename_info这里
+    std::string fatherPath = ROOT_PATH ;
+    std::string fileinfoPath = fatherPath + filename + "_info"; // "/raid/${file_name}_info"
+    const char* filesize_str = std::to_string(filesize).c_str();
+    int str_len = strlen(filesize_str);
+
+    int info_fd;
+    if ((info_fd = open(fileinfoPath.c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRWXU)) < 0) {    //创建信息文件
+        printf("%s_info create failed! \n",filename);
+        return -1;
+    }
+    truncate(fileinfoPath.c_str(), str_len);
+    void* info_ptr = mmap(NULL, str_len + 1, PROT_WRITE | PROT_READ, MAP_SHARED, info_fd, 0); 
+    if (info_ptr == MAP_FAILED) {
+        printf("mmap error: %s\n", strerror(errno));
+        return -2;
+    }
+    char* opt_for_info = (char*)info_ptr;
+
+    memcpy(opt_for_info,filesize_str,str_len);
+    if ((munmap(info_ptr, str_len + 1)) < 0) {
+       printf("unmap %s_info error \n",filename); 
+       return -3;
+    }
+
+    if (close(info_fd) < 0 ) {
+        printf("close %s_info error \n",filename);
+        return -4;
+    }
     /*
-         获取记录的文件信息里的 【文件大小】
-         单位：字节
-
-         成功返回 字节数
-         失败返回 -1
-     */
-    return 0; // REPLACE YOUR CODE HERE
+    int temp;
+    if ((temp = msync(info_ptr,str_len + 1,MS_ASYNC)) < 0) {
+        printf("%s write to disk failed! \n",filename);
+        return -4;
+    }
+    */
+    return 1;
 }
 
-// 磁盘编号，文件名
-void *file_get(int disk_id, const char *filename) {
-
-    /*
-
-     找到指定磁盘的指定文件
-
-     mmap映射
-
-     return ( mmap 返回的指针 )
-
-     成功返回 mmap指针
-     失败返回 nullptr
-     */
-    return nullptr;
-}
-
-// 设置文件基本信息
-void file_set(const char *filename, uint32_t filesize) {
-    // 记录文件信息到硬盘，给以后的 file_size() 使用
-
-    // 按照分块策略，在服务器创建好文件
-    // truncate 到指定大小
-    // fsync 刷新缓冲区
-
-    return;
-}
-
-// 写文件块数据
-
-// 磁盘编号 [0,12), 文件名，要写的数据缓冲区，要写多少字节
-int file_write(int disk_id, const char *filename, void *buffer, uint32_t length) {
-    //TODO: 思考题：多个客户端同时写一个文件怎么办？
-
-    /*
-
-     open(指定硬盘的指定文件)
-     mmap()
-     写入数据
-
-     */
-    return 0;
-}
-
-// 挂载磁盘（防止服务器磁盘没有挂载）
-int disk_mount(int disk_id) {
-    return 0;
-}
