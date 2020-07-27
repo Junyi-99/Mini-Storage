362cda918c00262230290bd6d10ec5b8fcf91b71
Elijah-F <Zhao_xiangshen@163.com>
2020-07-27 11:58:26 +0800

    fix(tcp_socket.hpp):

--- client/utils/tcp_socket.hpp
+++ client/utils/tcp_socket.hpp
@@ -33,87 +33,16 @@ public:
   bool Socket() {
     _fd = socket(AF_INET, SOCK_STREAM, 0);
 
+    int32_t snd_size = 4 * 1024 * 1024; // 4M
+    int32_t rcv_size = 4 * 1024 * 1024;
+    if (setsockopt(_fd, SOL_SOCKET, SO_SNDBUF, &snd_size, sizeof(socklen_t))) {
+      std::cerr << "设置发送缓冲区大小错误\n";
+    }
 
-      int err = -1;        /* 返回值 */
-      int snd_size = 0;   /* 发送缓冲区大小 */
-      int rcv_size = 0;    /* 接收缓冲区大小 */
-      socklen_t optlen;    /* 选项值长度 */
+    if (setsockopt(_fd, SOL_SOCKET, SO_RCVBUF, &rcv_size, sizeof(socklen_t))) {
+      std::cerr << "设置接收缓冲区大小错误\n";
+    }
 
-
-      /*
-       * 先读取缓冲区设置的情况
-       * 获得原始发送缓冲区大小
-       */
-      optlen = sizeof(snd_size);
-      err = getsockopt(_fd, SOL_SOCKET, SO_SNDBUF,&snd_size, &optlen);
-      if(err<0){
-          perror("error");
-          printf("获取发送缓冲区大小错误\n");
-      }
-      /*
-       * 打印原始缓冲区设置情况
-       */
-
-      /*
-       * 获得原始接收缓冲区大小
-       */
-      optlen = sizeof(rcv_size);
-      err = getsockopt(_fd, SOL_SOCKET, SO_RCVBUF, &rcv_size, &optlen);
-      if(err<0){
-          perror("error");
-          printf("获取接收缓冲区大小错误\n");
-      }
-
-      //printf(" 发送缓冲区原始大小为: %d 字节\n",snd_size);
-      //printf(" 接收缓冲区原始大小为: %d 字节\n",rcv_size);
-
-      /*
-       * 设置发送缓冲区大小
-       */
-      snd_size = 4096*1024;    /* 发送缓冲区大小为8K */
-      optlen = sizeof(snd_size);
-      err = setsockopt(_fd, SOL_SOCKET, SO_SNDBUF, &snd_size, optlen);
-      if(err<0){
-          printf("设置发送缓冲区大小错误\n");
-      }
-
-      /*
-       * 设置接收缓冲区大小
-       */
-      rcv_size = 4096*1024;    /* 接收缓冲区大小为8K */
-      optlen = sizeof(rcv_size);
-      err = setsockopt(_fd,SOL_SOCKET,SO_RCVBUF, (char *)&rcv_size, optlen);
-      if(err<0){
-          printf("设置接收缓冲区大小错误\n");
-      }
-
-      /*
-       * 检查上述缓冲区设置的情况
-       * 获得修改后发送缓冲区大小
-       */
-      optlen = sizeof(snd_size);
-      err = getsockopt(_fd, SOL_SOCKET, SO_SNDBUF,&snd_size, &optlen);
-      if(err<0){
-          printf("获取发送缓冲区大小错误\n");
-      }
-
-      /*
-       * 获得修改后接收缓冲区大小
-       */
-      optlen = sizeof(rcv_size);
-      err = getsockopt(_fd, SOL_SOCKET, SO_RCVBUF,(char *)&rcv_size, &optlen);
-      if(err<0){
-          printf("获取接收缓冲区大小错误\n");
-      }
-
-      /*
-       * 打印结果
-       */
-      //printf(" 发送缓冲区大小为: %d 字节\n",snd_size);
-      //printf(" 接收缓冲区大小为: %d 字节\n",rcv_size);
-
-
-      // TODO: setsockopt??
     return CHECK_RET(_fd, "socket error!");
   }
 
@@ -134,8 +63,7 @@ public:
     return CHECK_RET(res, "bind error!");
   }
 
-  // TODO: max = 10?
-  bool Listen(const int32_t backlog = 240) {
+  bool Listen(const int32_t backlog = 20) {
     int32_t res = listen(_fd, backlog);
     return CHECK_RET(res, "listen error!");
   }
